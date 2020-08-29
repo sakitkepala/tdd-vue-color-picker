20399bfbd579af661ede9a5451bad12dbc1ccbff
sakitkepala <andikapriyotama@gmail.com>
2020-08-29 18:34:15 +0700

    [ADD] Loader SVG di Vue & fixnya di Jest dan color-convert

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  transform: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.svg$': '<rootDir>/svgTransform.js'
+  },
 }
