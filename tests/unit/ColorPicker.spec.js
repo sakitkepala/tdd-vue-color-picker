cee3fca5622fa2e98272da37f99737fe7997b4d6
sakitkepala <andikapriyotama@gmail.com>
2020-08-30 12:59:54 +0700

    [MERAH] Ngetes kode warna yang dimounting swatch dengan propsnya

--- tests/unit/ColorPicker.spec.js
+++ tests/unit/ColorPicker.spec.js
@@ -17,19 +17,19 @@ describe('ColorPicker', () => {
 
     test('menampilkan masing-masing warna dalam bentuk swatch-swatch', () => {
       // Assert
-      // * catatan: ambil semua DOM swatch yang sudah dimounting di dalam komponen color picker
       const domSwatchSemua = wrapper.findAll('.swatch');
       expect(domSwatchSemua.length).toBeGreaterThan(0); // lulus
 
-      // dataProps.swatchSemua.forEach((swatch, index) => {
-      //   // * catatan: assertnya diloop sebanyak jumlah swatch di props
-      //   // habis itu dicocokkan styling warna yang ada di DOM
-      //   // dengan yang dioperkan lewat props
-      //   // * pertanyaan: apakah styling swatch di komponen pakai styling inline lewat attribut style HTML?
-      //   expect(domSwatchSemua.at(index).attributes().style).toBe(
-      //     `background-color: rgb(${convert.hex.rgb(swatch).join(', ')})`
-      //   );
-      // });
+      dataProps.swatchSemua.forEach((swatch, index) => {
+        // * catatan: fase merah lagi, kali ini untuk mencocokkan kode warna
+        // * catatan: assertnya diloop sebanyak jumlah swatch di props
+        // habis itu dicocokkan styling warna yang ada di DOM
+        // dengan yang dioperkan lewat props
+        // * pertanyaan: apakah styling swatch di komponen pakai styling inline lewat attribut style HTML?
+        expect(domSwatchSemua.at(index).attributes().style).toBe(
+          `background-color: rgb(${convert.hex.rgb(swatch).join(', ')});`
+        );
+      });
     });
 
   });
