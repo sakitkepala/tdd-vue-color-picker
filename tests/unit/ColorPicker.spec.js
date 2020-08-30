10f3bfb798ef4d44ad81c6ed8d28d940d86205aa
sakitkepala <andikapriyotama@gmail.com>
2020-08-30 12:32:11 +0700

    [RED] Memounting swatch-swatch di komponen

--- tests/unit/ColorPicker.spec.js
+++ tests/unit/ColorPicker.spec.js
@@ -1,7 +1,40 @@
 import { shallowMount } from '@vue/test-utils';
 import ColorPicker from '@/components/ColorPicker';
+import convert from 'color-convert';
+
 
 describe('ColorPicker', () => {
-    // cus
-    it('first', () => {});
+
+  let wrapper = null;
+  const dataProps = {
+    swatchSemua: ['e3342f', '3490dc', 'f6993f', '38c172', 'fff'],
+  };
+
+  beforeEach(() => ( wrapper = shallowMount(ColorPicker, { dataProps }) ));
+  afterEach(() => wrapper.destroy());
+
+  describe('SwatchSemua', () => {
+
+    test('menampilkan masing-masing warna dalam bentuk swatch-swatch', () => {
+      // Assert
+      // * catatan: ambil semua DOM swatch yang sudah dimounting di dalam komponen color picker
+      const domSwatchSemua = wrapper.findAll('.swatch');
+      // * catatan: di fase Merah, diharapkan punya DOM swatch yang sudah dimounting
+      // dan seharusnya gagal karena belum punya: komponennya belum punya template swatch
+      // yang mau dirender
+      expect(domSwatchSemua.length).toBeGreaterThan(0);
+
+      // dataProps.swatchSemua.forEach((swatch, index) => {
+      //   // * catatan: assertnya diloop sebanyak jumlah swatch di props
+      //   // habis itu dicocokkan styling warna yang ada di DOM
+      //   // dengan yang dioperkan lewat props
+      //   // * pertanyaan: apakah styling swatch di komponen pakai styling inline lewat attribut style HTML?
+      //   expect(domSwatchSemua.at(index).attributes().style).toBe(
+      //     `background-color: rgb(${convert.hex.rgb(swatch).join(', ')})`
+      //   );
+      // });
+    });
+
+  });
+
 });
