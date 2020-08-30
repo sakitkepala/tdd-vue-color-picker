23f3e510c15834e0865858c6f9bf8bb8648b9901
sakitkepala <andikapriyotama@gmail.com>
2020-08-30 13:07:52 +0700

    [HIJAU] Menjadikan tesnya lulus secara hardcoded di komponen

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <div class="swatch" />
+  <div class="color-picker">
+    <ul class="swatch-semua">
+      <li class="swatch" style="background-color: rgb(227, 52, 47)" />
+      <li class="swatch" style="background-color: rgb(52, 144, 220)" />
+      <li class="swatch" style="background-color: rgb(246, 153, 63)" />
+      <li class="swatch" style="background-color: rgb(56, 193, 114)" />
+      <li class="swatch" style="background-color: rgb(255, 255, 255)" />
+    </ul>
   </div>
 </template>
 
--- tests/unit/ColorPicker.spec.js
+++ tests/unit/ColorPicker.spec.js
@@ -21,11 +21,9 @@ describe('ColorPicker', () => {
       expect(domSwatchSemua.length).toBeGreaterThan(0); // lulus
 
       dataProps.swatchSemua.forEach((swatch, index) => {
-        // * catatan: fase merah lagi, kali ini untuk mencocokkan kode warna
-        // * catatan: assertnya diloop sebanyak jumlah swatch di props
-        // habis itu dicocokkan styling warna yang ada di DOM
-        // dengan yang dioperkan lewat props
-        // * pertanyaan: apakah styling swatch di komponen pakai styling inline lewat attribut style HTML?
+        // * catatan: hijau, DOM swatch dengan kode warna sesuai props di atribut style
+        // tapi masih hardcoded
+        // TODO: refaktor komponennya jadi dinamis untuk ambil warnanya dari props
         expect(domSwatchSemua.at(index).attributes().style).toBe(
           `background-color: rgb(${convert.hex.rgb(swatch).join(', ')});`
         );
